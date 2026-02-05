82208e7703b8b6974a43af65ed3295abc6096681
Jakob Rundlett <jakob.rundlett@gmail.com>
2026-02-05 13:57:48 +0100

    add mandlebrot skeleton

---
+++ mandlebrot.py
@@ -0,0 +1,21 @@
+"""
+Mandelbrot Set Generator
+Author : [Jakob Rundlett]
+Course : Numerical Scientific Computing 2026
+"""
+
+def f(x) :
+    """
+    Example function .
+    Parameters
+    ----------
+    x : float
+    Input value
+    Returns
+    -------
+    float
+    Output value
+    """
+    # TODO : Implement the algorithm
+    pass
+
